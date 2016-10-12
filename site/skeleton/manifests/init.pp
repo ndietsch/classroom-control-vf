d0a7153ddba1f7c505e026f5c565855b4bbedaf1
Nathan Dietsch <njd@ndietsch.com>
2016-10-12 14:42:28 -0500

    create skeleton class

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,11 @@
+class skeleton {
+
+  file { '/etc/skel':
+    ensure => directory,
+    before => File['/etc/skel/.bashrc'],
+    }
+    
+    file  { '/etc/skel/.bashrc':
+     ensure => file,
+     source => puppet://modules/skeleton/bashrc,
+     }
