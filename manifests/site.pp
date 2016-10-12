3f459d8e7d0b780e26e370a59fad335bf94e7157
Nathan Dietsch <njd@ndietsch.com>
2016-10-12 09:29:32 -0500

    Exercise 7.3
    Add testing.puppetlabs.vm host entry

--- manifests/site.pp
+++ manifests/site.pp
@@ -47,3 +47,10 @@ node default {
 
 #this is a comment
 # Second comment
+
+# Execrcise 7.3
+host { 'testing.puppetlabs.vm':
+  ensure => present,
+  name => testing.puppetlabs.vm,
+  ip => 127.0.0.1,
+  }
